5e8e3f1553a85734f203fde698129441152ac5a2
Carlos Arellano <carlosfarellano@hotmail.com>
2021-10-17 20:44:04 -0500

    Primer Commit

---
+++ doc/README.md
@@ -0,0 +1,4 @@
+ejecutar `npm run build` para compilar proyecto
+el código fuente se escribe en src y al compilar se deposita en dist
+`firebase emulators:start` para iniciar emuladores y testear el proyecto
+para más información, revisar la documentación de Firebase: https://firebase.google.com/docs
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    mode: 'development',
+    devtool: 'eval-source-map',
+
+    watchOptions: {
+        ignored: ['**/node_modules']
+    },
+
+    //Estos loaders sirven para incluir Boostrap 5 en el proyecto como una dependencia, pero ha habido
+    //problemas para hacerlo de esta forma, asi que se incluira como CDN por el momento.
+    /*
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: function() {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader'
+                }]
+            }
+        ]
+    }
+    */
+}
