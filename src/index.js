09727619d938d3633aa1fc06c8bef1612a16cb84
Carlos Arellano <carlosfarellano@hotmail.com>
2021-11-01 21:37:23 -0600

    Cambios de Path

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, connectAuthEmulator } from 'firebase/auth';
+import { 
+    initializeApp 
+} from "firebase/app";
+
+import { 
+    getAuth, 
+    GoogleAuthProvider, 
+    signInWithPopup, 
+    signOut, 
+    onAuthStateChanged, 
+    connectAuthEmulator 
+} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB59QXwzGzzlc1DlIq4bn-8rGYNl-YEFIQ",
   authDomain: "red-purificadores.firebaseapp.com",
   databaseURL: "https://red-purificadores-default-rtdb.firebaseio.com",
   projectId: "red-purificadores",
-  storageBucket: "red-purificadores.appspot.com",
+  storageBucket: "red-purificadores.appspot.com",   
   messagingSenderId: "465692295067",
   appId: "1:465692295067:web:63afb8986188b79c06eb3e",
   measurementId: "G-YQG5EHENT8"
